7ac19771e9aded18f892952e9a9e756e7b146516
yukun1992 <ychne152@ucr.edu>
2017-09-09 22:12:14 -0700

    fisrt version

--- js/app.js
+++ js/app.js
@@ -36,3 +36,168 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+let cards = ["fa fa-diamond", "fa fa-diamond",
+    "fa fa-paper-plane-o", "fa fa-paper-plane-o",
+    "fa fa-anchor", "fa fa-anchor",
+    "fa fa-bolt", "fa fa-bolt",
+    "fa fa-cube", "fa fa-cube",
+    "fa fa-leaf", "fa fa-leaf",
+    "fa fa-bicycle", "fa fa-bicycle",
+    "fa fa-bomb", "fa fa-bomb"];
+
+let cardsHtml = document.querySelectorAll('ul.deck > li');
+let openCards = [];
+let numberOfFailedMoves = 0;
+let numberOfMoves = 0;
+
+let movesElement = document.getElementsByClassName("moves")[0];
+
+let starsElements = document.querySelectorAll(".stars > li > i");
+
+document.getElementsByClassName("restart")[0].addEventListener('click', resetGame);
+
+const timeToShowCardInMs = 200;
+
+let timer = null;
+let startTime = new Date().getTime();
+let currentTime;
+
+function resetGame() {
+    cards = shuffle(cards);
+    addCardsInHtml(cards, cardsHtml);
+    turnCardsFaceDown();
+    resetVariables();
+    resetTimer();
+    startTimer();
+}
+
+function addCardsInHtml(cards, cardsHtml) {
+    for(i = 0; i < cards.length; i++) {
+        cardsHtml[i].firstElementChild.className = cards[i];
+    }
+}
+
+function turnCardsFaceDown() {
+    cardsHtml.forEach(function(e) {
+        e.className = "card";
+    });
+}
+
+function resetVariables() {
+    openCards = [];
+    numberOfFailedMoves = 0;
+    numberOfMoves = 0;
+    starsElements.forEach(function(e) {
+        e.className = "fa fa-star";
+    });
+    updateMovesElement();
+}
+
+function updateMovesElement() {
+    movesElement.innerHTML = numberOfMoves + " Steps";
+}
+
+function resetTimer() {
+    startTime = new Date().getTime();
+}
+
+// convert unix time to real time
+function startTimer() {
+    timer = setInterval(function() {
+        let now = new Date().getTime();
+        let elapsed = now - startTime;
+        let minutes = Math.floor((elapsed % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((elapsed % (1000 * 60)) / 1000);
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        currentTime = minutes + ':' + seconds;
+        let clock = document.getElementsByClassName("clock")[0];
+        clock.innerHTML = "Time: " + currentTime;
+    }, 1000);
+}
+
+function addEventListenerToCards() {
+    cardsHtml.forEach(function(e) {
+        e.addEventListener('click', function() {
+            if (this.className === "card") {
+                displayCard(this);
+                addCardOpenCards(this);
+                checkWinConditionAndAct();
+            }
+        });
+    });
+}
+
+function displayCard(card) {
+    card.className = "card open show";
+}
+
+function addCardOpenCards(card) {
+    openCards.push(card);
+}
+
+function checkWinConditionAndAct() {
+    if (openCards.length == 2) {
+        if (checkCardsEqual()) {
+            changeOpenCardsClassWhenMatch();
+        } else {
+            changeOpenCardsClassWhenNotMatch();
+            numberOfFailedMoves++;
+            updateStarts();
+        }
+        openCards = [];
+        numberOfMoves++;
+        updateMovesElement();
+        isGameOver();
+    }
+}
+
+function checkCardsEqual() {
+    return openCards[0].firstElementChild.className === openCards[1].firstElementChild.className;
+}
+
+function changeOpenCardsClassWhenMatch() {
+    var lastOpenCards = openCards;
+    setTimeout(function() {
+        lastOpenCards[0].className = "card match";
+        lastOpenCards[1].className = "card match";
+    }, timeToShowCardInMs);
+}
+
+function changeOpenCardsClassWhenNotMatch() {
+    var lastOpenCards = openCards;
+    setTimeout(function() {
+        lastOpenCards[0].className = "card notMatch";
+        lastOpenCards[1].className = "card notMatch";
+        setTimeout(function() {
+            lastOpenCards[0].className = "card";
+            lastOpenCards[1].className = "card";
+        }, timeToShowCardInMs);
+    }, timeToShowCardInMs);
+}
+
+function updateStarts() {
+    if (numberOfFailedMoves == 3) {
+        starsElements[2].className = "fa fa-star disabled";
+    }
+    else if (numberOfFailedMoves == 6) {
+        starsElements[1].className = "fa fa-star disabled";
+    }
+    else if (numberOfFailedMoves == 9) {
+        starsElements[0].className = "fa fa-star disabled";
+    }
+}
+
+function isGameOver() {
+    setTimeout(function() {
+        cardElements = document.querySelectorAll('.card:not(.match)');
+        if (cardElements.length === 0) {
+            alert("You win!!! Total steps: " + numberOfMoves);
+            resetGame();
+        }
+    }, timeToShowCardInMs * 2);
+}
+
+resetGame();
+addEventListenerToCards();
